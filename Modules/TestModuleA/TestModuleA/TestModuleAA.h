83f9c1cc02f507db9387de111f1e852365bc71b7
孙奥 <sunao@staff.tixa.com>
2024-04-30 17:03:35 +0800

    1212

---
+++ Modules/TestModuleA/TestModuleA/TestModuleAA.h
@@ -0,0 +1,16 @@
+//
+//  TestModuleAA.h
+//  TestModuleA
+//
+//  Created by PF on 2024/4/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModuleAA : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Modules/TestModuleA/TestModuleA/TestModuleAA.m
@@ -0,0 +1,12 @@
+//
+//  TestModuleAA.m
+//  TestModuleA
+//
+//  Created by PF on 2024/4/30.
+//
+
+#import "TestModuleAA.h"
+
+@implementation TestModuleAA
+
+@end
